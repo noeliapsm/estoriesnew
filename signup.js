6de9127ac6786ebefa0411178b403adc40493be8
Noelia Poblete <128185845+noeliapsm@users.noreply.github.com>
2024-05-17 13:17:40 +0200

    Create signup.js

---
+++ signup.js
@@ -0,0 +1,22 @@
+document.getElementById('signupForm').addEventListener('submit', async function(event) {
+    event.preventDefault();
+
+    const username = document.getElementById('username').value.trim();
+    const password = document.getElementById('password').value.trim();
+
+    // Envía los datos del formulario al servidor para crear una nueva cuenta de usuario
+    const response = await fetch('http://localhost:3000/signup', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ username, password })
+    });
+
+    if (response.ok) {
+        alert('User registered successfully!');
+        window.location.href = 'stories.html'; // Redirige al usuario a la página de creación de historias
+    } else {
+        alert('Error registering user. Please try again.');
+    }
+});
